cfc21c65eadcbba67c6e4ab7c34613e609a79c9a
fightingyu <iwillsaynothing@163.com>
2018-01-31 16:23:38 +0800

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+var state = {
+  count: 10
+}
+
+const mutations = {
+  increment(state) {
+    state.count++
+  },
+  decrement(state) {
+    state.count--
+  },
+}
+
+const actions = {
+  increment:({commit}) => {
+    commit('increment')
+  },
+  decrement:({commit}) => {
+    commit('decrement')
+  },
+  clickOdd:({commit, state}) => {
+    if (state.count % 2 === 0) {
+      commit('increment')
+    }
+  },
+  clicknysc:({commit}) => {
+    new Promise((resolve)=> {
+      setTimeout(function(){
+        // alert(1)
+        commit('increment')
+      }, 2000)
+    })
+  }
+}
+
+const getters = {
+  count(state) {
+    return state.count
+  },
+  getOdd(state) {
+    return state.count%2===0?'偶数':'奇数'
+  }
+}
+
+
+export default new Vuex.Store({
+  actions,
+  state,
+  mutations,
+  getters
+})
